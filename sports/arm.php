1016226efc89a0aa5c8a68b30c84c2748a7a9787
Nick <nikeca17_kn@abv.bg>
2017-04-04 23:17:41 +0300

    sign up

--- sports/arm.php
+++ sports/arm.php
@@ -1,26 +1,36 @@
-<?php 
-    if(isset($_POST['fname1']) and isset($_POST['lname1']) and isset($_POST['mail1'])){
-    $to1 = "aubgolympicscommittee@gmail.com"; // this is your Email address
-    
-    $from1 = $_POST['mail1']; // this is the sender's Email address
+<?php
+if(isset($_POST['fname1']) && isset($_POST['lname1']) && isset($_POST['email1'])){
+$to = "aubgolympicscommittee@gmail.com"; // this is your Email address
+       
+      
+      $from1 = $_POST['mail1']; // this is the sender's Email address
     $first_name1 = $_POST['fname1'];
     $last_name1 = $_POST['lname1'];
-
     
-			if($from1 == '' or $first_name1 == '' or $last_name1 == '') {
-  			 echo "Error";
-   				exit();
-   				} else {
-   				 $subject = "Arm Wrestling";
-  				  $message = $first_name1 . " " . $last_name1 . "\n";
-   					 	$headers = "From:" . $from1;
-  				  		$headers2 = "From:" . $to;
-  				  		mail($to1,$subject,$message,$headers);
-    					echo "Mail Sent. Thank you for registering!";
-    					// You can also use header('Location: thank_you.php'); to redirect to another page.
-    		}
+    $subject = "Arm Wrestling";
+  	$message = "Player 1: ". $first_name1 . " " . $last_name1 . " ". $from1. PHP_EOL;
+
+    $headers = "From:" . $from1;
+  	mail($to,$subject,$message,$headers);
+    try{
+    $file = fopen("/www/aubgolympics.com/signups/arm.txt", 'ab');
+    fwrite($file, "===========START===========".PHP_EOL);
+    fwrite($file, $message);
+    fwrite($file, "============END============".PHP_EOL);
+    fclose($file);
+    }
+    catch(Exception $e){
+      
+    }
+    echo "Thank you for registering! You will now be redirected";
+    echo "<script>";
+    echo "window.location.href='http://www.aubgolympics.com/sports.html.com';</script>";
+    die();
     } else {
     echo "Empty Fields";
     }
    
 ?>
+
+
+ 
